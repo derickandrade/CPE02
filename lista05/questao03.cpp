9da73742720752753108314619c80481791434f0
Dérick <120838145+derickandrade@users.noreply.github.com>
2024-12-18 10:26:31 -0300

    Update questao03.cpp

--- lista05/questao03.cpp
+++ lista05/questao03.cpp
@@ -1,41 +1,38 @@
 #include <stdio.h>
-#include <cmath>
-bool primo_ateRaiz(int numero) { //verifico até a raiz, aqui dá pra ver, por exemplo que se o número for 9, a metade estaria entre 4 e 5, mas a raiz que é 3, já é um múltiplo
-    int raiz = sqrt(numero);
-    int flag = 0;
-    for (int i = 2; i <= raiz; i++){
-       if ((numero % i) == 0){
-           flag++;
-       }
+
+bool perfect(int numero){
+    int acumulador = 1;
+    int fator = 2;
+
+    for (int i = 2; i <= numero / 2; i++){
+        if (numero % i == 0){
+            acumulador += i;
+        }
     }
-    if (flag < 1){
+
+    if (acumulador == numero){
         return true;
     }
     return false;
 }
 
-void perfect(int numero){
+void imprimeFatores(int numero){
     int acumulador = 1;
     int fator = 2;
-    int original = numero;
-    do{
-        while(!(primo_ateRaiz(fator))){
-            fator++;
-        }
-        if ((numero % fator) == 0){
-            acumulador += fator;
-            printf("fator: %d acum: %d\n", fator, acumulador);
-            numero /= fator;
-            
+    printf("1");
+    for (int i = 2; i <= numero / 2; i++){
+        if (numero % i == 0){
+            acumulador += i;
+            printf(" + %d", i);
         }
     }
-    while(numero != 1);
-    if (acumulador == original){
-        printf("%d\nfator: %d acumulador: %d", original, fator, acumulador);
-    }
-    printf("%d\nfator: %d acumulador: %d", original, fator, acumulador);
+    printf(" = %d\n", numero);
 }
 
 int main(){
-    perfect(6);
+    for (int i = 2; i < 33550337; i++){
+        if (perfect(i)){
+            imprimeFatores(i);
+        }
+    }
 }
